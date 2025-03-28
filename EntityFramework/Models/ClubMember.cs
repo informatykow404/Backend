12dac6d686d614d750d812b1afe37c10686fed24
Dorian Koehler <pixel12pix@gmail.com>
2025-03-28 16:18:15 +0100

    feat: add missing table

---
+++ EntityFramework/Models/ClubMember.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Backend.EntityFramework.Models.Enums;
+
+namespace Backend.EntityFramework.Models;
+
+public class ClubMember
+{
+    [Key]
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public required User User { get; set; }
+    public required ScienceClub Club { get; set; }
+    public ScienceClubRole Role { get; set; }
+}
+
